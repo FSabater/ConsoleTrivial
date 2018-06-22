8dd6f8ee099b3fb85b2e036b60f2b97e03a5f1b9
DESKTOP-G9VOQRN\Otakon <fsabatervillora@gmail.com>
2018-06-22 13:20:19 +0200

    Version 0.01 ConsoleTrivial

---
+++ ConsoleTrivial.cs
@@ -0,0 +1,199 @@
+﻿// Francisco Sabater Villora
+
+// Version 01: Load and answer questions
+
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+public struct Question
+{
+    public string text;
+    public string category;
+    public string answer1;
+    public string answer2;
+    public string answer3;
+    public string answer4;
+    public ushort correct;
+}
+
+public struct Player
+{
+    public string name;
+    public ushort score; 
+}
+
+class ConsoleTrivial
+{
+    public static String[] ReadFile(string fileName)
+    {
+        try
+        {
+            return File.ReadAllLines(fileName);
+        }
+        catch (PathTooLongException)
+        {
+            Console.WriteLine("Path too long");
+            String[] error = new string[0];
+            return error;
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine("File not accessible");
+            String[] error = new string[0];
+            return error;
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine("I/O error: " + e.Message);
+            String[] error = new string[0];
+            return error;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error: " + e.Message);
+            String[] error = new string[0];
+            return error;
+        }
+    }
+
+    public static void PlayersSelect(ref ushort totalPlayers,
+        ref List<Player> playersList)
+    {
+        bool validNumPlayers = false;
+        while (!validNumPlayers)
+        {
+            Console.WriteLine("How many players? choose between 2 - 4");
+            string input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "2":
+                case "3":
+                case "4":
+                    totalPlayers = Convert.ToUInt16(input);
+                    validNumPlayers = true;
+                    break;
+                default:
+                    Console.WriteLine("Invalid input, choose a valid option");
+                    break;
+            }
+        }
+
+        for (ushort i = 0; i < totalPlayers; i++)
+        {
+            Console.Write("Enter player {0} name: ", i + 1);
+            string nameAux = Console.ReadLine();
+            Player aux = new Player();
+            aux.name = nameAux;
+            aux.score = 0;
+            playersList.Add(aux);
+        }
+    }
+
+    public static void QuestionShow(ref ushort points,
+        string[] allFileLines)
+    {
+        Random rnd = new Random();
+        int questionNumber = rnd.Next(0, allFileLines.Length);
+        string line = allFileLines[questionNumber];
+        Question actualQuestion = new Question();
+        string[] questionArray = line.Split('#');
+        actualQuestion.category = questionArray[0];
+        actualQuestion.text = questionArray[1];
+        actualQuestion.answer1 = questionArray[2];
+        actualQuestion.answer2 = questionArray[3];
+        actualQuestion.answer3 = questionArray[4];
+        actualQuestion.answer4 = questionArray[5];
+        actualQuestion.correct = Convert.ToUInt16(questionArray[6]);
+
+        Console.WriteLine(actualQuestion.text);
+        Console.WriteLine(actualQuestion.answer1);
+        Console.WriteLine(actualQuestion.answer2);
+        Console.WriteLine(actualQuestion.answer3);
+        Console.WriteLine(actualQuestion.answer4);
+        Console.Write("Enter the question number: ");
+
+        string userInput = Console.ReadLine();
+        if (userInput == Convert.ToString(actualQuestion.correct))
+        {
+            Console.WriteLine("Correct!");
+            points += 1; 
+        }
+        else
+        {
+            Console.WriteLine("Incorrect!");
+        }
+
+        Console.WriteLine("Press Enter to end turn");
+        Console.ReadLine();
+    }
+
+    public static void ShowScores(List<Player> playersList)
+    {
+        foreach (Player p in playersList)
+        {
+            Console.Write("{0} - ",
+                p.name);
+            Console.WriteLine("Score: {0}",
+                p.score);
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        string fileName = "txt/questions.txt";
+        string[] allFileLines = ReadFile(fileName);
+        List<Player> playersList = new List<Player>();
+
+        bool exit = false;
+        ushort totalPlayers = 0;
+
+        // players select
+        PlayersSelect(ref totalPlayers, ref playersList);
+        
+
+        // Game Loop
+        ushort actualPlayer = 0;
+        do
+        {
+            Console.Clear();
+            ShowScores(playersList);
+
+            Console.WriteLine();
+            Console.WriteLine("{0} playing... ", 
+                playersList[actualPlayer].name);
+            ushort actualScore = playersList[actualPlayer].score;
+            // Question Show
+            QuestionShow(ref actualScore, allFileLines);
+
+            /*  The next code is for update the player score
+                            Alert !
+                Change the next lines of code if the structcs remove
+            */
+            Player aux = new Player();
+            aux.name = playersList[actualPlayer].name;
+            aux.score = actualScore;
+            playersList[actualPlayer] = aux;
+
+            if(actualScore == 20)
+            {
+                Console.Clear();
+                Console.WriteLine("{0} wins!", 
+                    playersList[actualPlayer].name);
+                Console.WriteLine("Results: ");
+                ShowScores(playersList);
+                Console.WriteLine("Press Enter for exit");
+                Console.ReadLine();
+                exit = true;
+            }
+
+            actualPlayer++;
+            if(actualPlayer == totalPlayers)
+            {
+                actualPlayer = 0;
+            }
+        } while (!exit);
+    }
+}
+
---
+++ docs/version01.md
@@ -0,0 +1,53 @@
+# Console Trivial
+
+## Version 01: Load and answer questions
+
+You must create a text, with questions belonging to 4 categories (Programming, 
+Databases, Systems, Web). For each question there will be: text, category, 4 
+possible answers, number of the correct answer. The content will be loaded from 
+the file at the beginning of each game session.
+
+When starting a game, the number of players (up to a maximum of 4) and the name 
+of each player will be asked. A game session will start, in which, each turn, a 
+question will be chosen at random and it will be shown to the player who has 
+the turn. Their answer will be taken and checked. Even if successful, the turn 
+will always change to the next player. Each successfully answered question will 
+add a point. The game will end when one player has 20 points.
+
+The game will finally (after several versions) look similar to this:
+
+![](trivialConsole.png)
+
+
+The interface can be in Spanish or English, but the source (names of variables, 
+comments, etc) must be in English.
+
+The classes structure is not important yet, because that will be formalized in a 
+later version.
+
+---
+
+## Entrega 01: Cargar y responder preguntas
+
+Se creará un fichero de texto con algunas preguntas pertenecientes a 4 
+categorías (Programming, Databases, Systems, Web). Para cada pregunta habrá: 
+enunciado, categoría, 4 posibles respuestas, número de la respuesta correcta. 
+El contenido se cargará desde fichero al principio de cada sesión de juego. 
+
+Al comenzar una partida, se preguntará la cantidad de jugadores (hasta un 
+máximo de 4) y el nombre a cada jugador. Comenzará una sesión de juego, en la 
+que en cada turno se escogerá una pregunta al azar y se le mostrará al que 
+tenga el turno. Se tomará su respuesta y se comprobará si ha acertado. Aunque 
+haya acertado, el turno siempre cambiará de jugador.  Cada pregunta acertada 
+sumará un punto, y las equivocadas no restarán. Se repetirá hasta que una ronda 
+termine y un jugador tenga 20 puntos.
+
+La apariencia final, hacia la que te irás acercando en sucesivas aproximaciones será algo como:
+
+![](trivialConsole.png)
+
+El interfaz puede estar en castellano o en inglés, pero el fuente (nombres
+de variables, comentarios, etc) debe estar en inglés.
+
+Todavía no es importante la estructura de clases, porque eso se formalizará
+en una entrega posterior.
